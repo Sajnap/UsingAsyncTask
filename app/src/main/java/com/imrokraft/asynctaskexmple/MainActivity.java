6434e97e883812010517c523baa2da30c460c37d
sajnap <sajnap81@gmail.com>
2015-03-09 10:27:29 +0530

    first commit

---
+++ app/src/main/java/com/imrokraft/asynctaskexmple/MainActivity.java
@@ -0,0 +1,46 @@
+package com.imrokraft.asynctaskexmple;
+
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+    int count=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public  void FirstClick(View v){
+        Toast.makeText(getApplicationContext(),"first click",Toast.LENGTH_LONG).show();
+        startActivity(new Intent(this,SecMainActivity.class));
+    }
+    public  void TwoClick(View v){
+        Thread th=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (; ; ) {
+                    System.out.println("Thread" + count++);
+                    if (count > 1000) {
+                        break;
+                    }
+
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Finished the loops", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+        th.start();
+
+    }
+}
